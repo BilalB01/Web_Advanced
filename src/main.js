01abe200f6d3e2bc2c0154fc529914ce301e074d
BilalB01 <bilal.bouchta@student.ehb.be>
2025-05-18 23:19:49 +0200

    zoekbalk toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,37 +1,26 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("character-container");
+import { activeerZoekfunctie } from './zoekbalk.js';  // hier zorg ik dat de zoekfunctie uit de zoekbalk.js geimorteerd wordt
 
-  // omdat ik 60 personnages wil in mijn homepage moet ik 3 pagina's roepen per pagina kon ik maar 20 personnages tonen 
-  Promise.all([
-    fetch("https://rickandmortyapi.com/api/character?page=1").then(res => res.json()),
-    fetch("https://rickandmortyapi.com/api/character?page=2").then(res => res.json()),
-    fetch("https://rickandmortyapi.com/api/character?page=3").then(res => res.json())
-  ])
-    .then(([data1, data2, data3]) => {
-      // hier combineer ik de drie arrays tot 1 array om de 60 personages te hebben
-      const characters = [...data1.results, ...data2.results, ...data3.results];
+document.addEventListener('DOMContentLoaded', async () => {
+  const container = document.getElementById('character-container'); // hier zorg ik dat Dom-elementen geselecteerd worden om er later ee te werken
+  const zoekInput = document.getElementById('zoekveld');
 
-      characters.forEach(char => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        // hier ga ik de 6 items toevoegen 
-        card.innerHTML = ` 
-          <h3>${char.name}</h3>
-          <img src="${char.image}" alt="${char.name}">
-          <p><strong>Status:</strong> ${char.status}</p>
-          <p><strong>Soort:</strong> ${char.species}</p>
-          <p><strong>Geslacht:</strong> ${char.gender}</p>
-          <p><strong>Afkomst:</strong> ${char.origin.name}</p>
-        `;
-        container.appendChild(card);
-      });
-    })
-    // hier is het als er een fout is dan zal er een foutmelding tonen 
-    .catch(error => {
-      container.innerHTML = `<p>Er ging iets mis: ${error.message}</p>`;
-      console.error(error);
-    });
+  try {
+    const responses = await Promise.all([
+      fetch("https://rickandmortyapi.com/api/character?page=1"),
+      fetch("https://rickandmortyapi.com/api/character?page=2"),
+      fetch("https://rickandmortyapi.com/api/character?page=3")
+    ]);
+
+    const data = await Promise.all(responses.map(res => res.json()));
+    const allePersonages = [...data[0].results, ...data[1].results, ...data[2].results];
+
+    // Start   de zoekfunctionaliteit met de opgehaalde date en inputveld 
+    activeerZoekfunctie(allePersonages, zoekInput, container);
+  } catch (error) { // als er iets fout is dan wordt dit getoond 
+    container.innerHTML = `<p>Er is iets fout gegaan: ${error.message}</p>`;
+  }
 });
+
   
   
